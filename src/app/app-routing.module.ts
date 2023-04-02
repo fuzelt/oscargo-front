34763c816268e9651803511dabce850b76705b4e
fabio <fabiouz@gmail.com>
2023-04-02 12:48:06 -0300

    home and map added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'map/googlemap',
+    loadChildren: () => import('./pages/map/googlemap/googlemap.module')
+      .then(m => m.GooglemapModule)
+  },
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
